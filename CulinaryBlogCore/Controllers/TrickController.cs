f89db0bcb25a06cc4901a9dc0da729df192ca469
IvayloIV <ivvo98@abv.bg>
2020-08-11 18:27:29 +0300

    Init project

---
+++ CulinaryBlogCore/Controllers/TrickController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CulinaryBlogCore.Controllers
+{
+    public class TrickController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CulinaryBlogCore/wwwroot/js/navigation.js
@@ -0,0 +1,9 @@
+﻿document.getElementById('receipt-menu').addEventListener('click', function () {
+    let element = document.getElementsByClassName('header-inner-nav')[0];
+
+    if (!element.classList.contains("clicked")) {
+        element.classList.add("clicked");
+    } else {
+        element.classList.remove("clicked");
+    }
+});
